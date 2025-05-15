f4fd608e00753d7b85a3af54c861aeb76380e12a
Hlib <glebmeltsoff@gmail.com>
2025-05-15 04:26:47 +0300

    update lots.js

--- routes/lots.js
+++ routes/lots.js
@@ -1,14 +1,36 @@
+// routes/lots.js
 const express = require('express');
-const lotsController = require('../controllers/lots');
-
 const router = express.Router();
+// Імпортуємо контролер з файлу 'lots.js'
+const lotsController = require('../controllers/lots.js'); // <--- ОСЬ ТУТ ЗМІНА
 
+// --- Маршрути для лотів ---
+
+// GET /lots -> Отримати всі лоти (з пагінацією та можливістю фільтрації/пошуку через query params)
 router.get('/', lotsController.getLots);
-router.put('/:lotId/status', lotsController.updateLotStatus);
-router.get('/user/:userId', lotsController.getLotsByUserId);
-router.get('/:lotId', lotsController.getSingleLot);
+
+// POST /lots/create -> Створити новий лот
+// (Якщо ви вирішите змінити на POST /lots, оновіть і тут, і на фронтенді)
 router.post('/create', lotsController.createSingleLot);
-router.post('/search', lotsController.searchLotByTitle);
-router.delete('/:lotId', lotsController.deleteSingleLot);
+
+// GET /lots/search -> Пошук лотів за назвою (title як query parameter)
+// Переконайтеся, що метод searchLotByTitle в controllers/lots.js тепер читає req.query.title
+router.get('/search', lotsController.searchLotByTitle);
+
+// GET /lots/user/:userId -> Отримати лоти конкретного користувача
+// Цей маршрут більш специфічний, тому ставимо його перед /:lotId
+router.get('/user/:userId', lotsController.getLotsByUserId);
+
+// GET /lots/:lotId -> Отримати один лот за ID
+router.get('/:lotId', lotsController.getSingleLot);
+
+// PUT /lots/:lotId -> Оновити інформацію про лот
 router.put('/:lotId', lotsController.updateSingleLot);
+
+// PUT /lots/:lotId/status -> Оновити тільки статус лоту
+router.put('/:lotId/status', lotsController.updateLotStatus);
+
+// DELETE /lots/:lotId -> Видалити лот
+router.delete('/:lotId', lotsController.deleteSingleLot);
+
 module.exports = router;
